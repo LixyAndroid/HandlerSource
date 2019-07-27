5b575db61e5ea1cac5aa8f954b6bade8bd27fb20
xuyang-happy <958607524@qq.com>
2019-07-27 23:31:36 +0800

    first commit

--- src/com/xuyang/handlersource/HandlerTest.java
+++ src/com/xuyang/handlersource/HandlerTest.java
@@ -2,6 +2,10 @@ package com.xuyang.handlersource;
 
 public class HandlerTest {
 	public static void main(String[] args) {
+
+
+        //1，主线程的Looper对象自动生成，不需手动生成；而子线程的Looper对象则需手动通过Looper.prepare()创建
+        //2，在子线程若不手动创建Looper对象 则无法生成Handler对象
 		Looper.prepare();
 
         ActivityThread thread = new ActivityThread();
